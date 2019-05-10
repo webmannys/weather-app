91bc4b8da175bf5ea2b47fdc484929992aabf206
Manny Sangha <webmannys@gmail.com>
2019-05-09 20:30:11 -0700

    Added more fields: min-temp, max-temp, humidity, sunrise, sunset.

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
+var titleText = 'Weather App';
 var apiKey = '3b06482364a722da5644982bf3f788dc';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {title: 'Weather App', city: null, temp: null});
+  res.render('index', {title: titleText, city: null, temp: null, temp_min: null, temp_max: null, humidity: null, sunrise: null, sunset: null});
 });
 
 /* POST home page. */
@@ -22,7 +23,7 @@ router.post('/', function (req, res, next) {
         res.render('index', {weather: null, error: 'Error, please try again'});
       } else {
 				console.log(weather);
-        res.render('index', {title: 'Weather App', city: weather.name, temp: weather.main.temp});
+        res.render('index', {title: titleText, city: weather.name, temp: weather.main.temp, temp_min: weather.main.temp_min, temp_max: weather.main.temp_max, humidity: weather.main.humidity, sunrise: weather.sys.sunrise, sunset: weather.sys.sunset});
       }
     }
   });
